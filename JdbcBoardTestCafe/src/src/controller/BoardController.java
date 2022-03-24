04b2bc427b40fa4ce0ce1982411a1e4d7b3de4e6
TAEPAPER <82808084+TAEPAPER@users.noreply.github.com>
2022-03-24 10:43:06 +0900

    Update BoardController.java

--- JdbcBoardTestCafe/src/src/controller/BoardController.java
+++ JdbcBoardTestCafe/src/src/controller/BoardController.java
@@ -19,13 +19,10 @@ public class BoardController {
 		while (true) {
 			int num = displayMenu();
 			switch(num) {
-			
 			case 1: newBoard();break;
 			case 2:break;
 			case 3:break;
 			case 0: break;
-			
-			
 			
 			}
 			
@@ -38,6 +35,7 @@ public class BoardController {
 		System.out.println("새글 작성하기");
 		System.out.println("---------//-----------");
 		System.out.print("제 목 : ");
+		scan.nextLine(); //버퍼 비워주기
 		String title = scan.nextLine();
 		String writer = scan.nextLine();
 		String content = scan.nextLine();
@@ -47,7 +45,7 @@ public class BoardController {
 		memVo.setBoard_writer(writer);
 		memVo.setBoard_content(content);
 		int cnt = service.insertBoard(memVo);
-		if(cnt>0)System.out.println("새글이 추가되었습니");
+		if(cnt>0)System.out.println("새글이 추가되었습니다");
 		
 		
 	}
@@ -57,11 +55,15 @@ public class BoardController {
 		System.out.println("No\tt제 목\t\t작 성 자\t\t조회");
 		System.out.println("------------------------------------------");
 		List<BoardMemVO> list = service.displayAll();
+		if(list == null || list.size()==0){
+		     "출력할 게시글이 하나도 없습니다"
+		}else{
 		for(BoardMemVO mem : list) {
 		 System.out.print(mem.getBoard_no()+"\t");
 		 System.out.print(mem.getBoard_title()+"\t");
 		 System.out.print(mem.getBoard_writer()+"\t");
 		 System.out.println(mem.getBoard_cnt()+"\t");
+			}
 		}
 		System.out.println("------------------------------------------");
 		System.out.println("1.새글작성  2.게시글 보기  3.검색  4. 작업끝  ");
