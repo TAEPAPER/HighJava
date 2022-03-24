81a6aea42867cb4f72ea49d062500190eccd92b8
TAEPAPER <82808084+TAEPAPER@users.noreply.github.com>
2022-03-24 09:38:30 +0900

    Update BoardMemDaoImpl.java

--- JdbcBoardTestCafe/src/src/dao/BoardMemDaoImpl.java
+++ JdbcBoardTestCafe/src/src/dao/BoardMemDaoImpl.java
@@ -30,7 +30,7 @@ public class BoardMemDaoImpl implements BoardMemDao {
 
 	@Override
 	public List<BoardMemVO> displayAll(Connection conn) throws SQLException {
-		String sql = "select * from jdbc_board ";
+		String sql = "select * from jdbc_board order by board_no desc";
 		conn = DBUtil3.getConnection();
 		PreparedStatement pstmt = conn.prepareStatement(sql);
 		ResultSet rs = pstmt.executeQuery();
@@ -53,35 +53,35 @@ public class BoardMemDaoImpl implements BoardMemDao {
 	@Override
 	public int insertBoard(Connection conn, BoardMemVO boardMemVo) throws SQLException  {
 		String sql = "insert into jdbc_board values(board_seq.nextVal,?,?,"
-				+ " sysdate,NVL(max(board_cnt)+1,0),?)  ";
+				+ " sysdate,0),?)  ";
 		conn = DBUtil3.getConnection();	
 		PreparedStatement pstmt= conn.prepareStatement(sql);
 		pstmt.setString(1,boardMemVo.getBoard_title());
 		pstmt.setString(2,boardMemVo.getBoard_writer());
 		pstmt.setString(3,boardMemVo.getBoard_content());
 		int cnt = pstmt.executeUpdate();
-		
+		if(pstmt!=null)pstmt.close();
 		return cnt;
 	}
 
 	@Override   //해당 게시글 보기
 	public List<BoardMemVO> choiceBoard(Connection conn,int boardNo ) throws SQLException {
-		 String sql = "select * from jdbc_board where board_no = ?";
+		 String sql = "select jdbc_no,board_title,board_writer, to_char(board_date,''yyyy-mm-dd') board_date,board_cnt, board_ content from jdbc_board where board_no = ?";
 		 conn = DBUtil3.getConnection();
 		   PreparedStatement  pstmt= conn.prepareStatement(sql);
 		   pstmt.setInt(1,boardNo);
-		   	 ResultSet rs = pstmt.executeQuery();
-		   	 List<BoardMemVO> list = new ArrayList<BoardMemVO>();
+		   ResultSet rs = pstmt.executeQuery();
+		  List<BoardMemVO> list = new ArrayList<BoardMemVO>();
 		   	  if(rs.next()) {
 		   		    BoardMemVO memVo = new BoardMemVO();
 		  
 		   		    memVo.setBoard_no(rs.getInt("board_no"));
-		   		    memVo.setBoard_writer(rs.getString("board_title"));
+		   		    memVo.setBoard_title(rs.getString("board_title"));
 		   		    memVo.setBoard_writer(rs.getString("board_writer"));
 		   		    memVo.setBoard_content(rs.getString("board_content"));
 		   		    memVo.setBoard_date("board_date");
 		   		    memVo.setBoard_cnt("board_cnt");
-		   		    	list.add(memVo);                        
+		   		    list.add(memVo);                        
 		   	  }
 		return list;
 	}
@@ -89,10 +89,10 @@ public class BoardMemDaoImpl implements BoardMemDao {
 	@Override
 	public int updateBoard(Connection conn, String title, String content ,int boardNo) {
 		//제목,내용만 수정 가
-		String sql = "update jdbc_board set board_title = ? ,"
+		String sql = "update jdbc_board set board_title = ? board_date = sysdate ,"
 				+ " board_content =?  where board_no = ?";
 		conn= src.util.DBUtil3.getConnection();
-				pstmt = conn.prepareStatement(sql);
+		pstmt = conn.prepareStatement(sql);
 		 pstmt.setString(1,title);
 		 pstmt.setString(2, sql);
 		 pstmt.setInt(3, boardNo);
@@ -103,23 +103,23 @@ public class BoardMemDaoImpl implements BoardMemDao {
 
 	@Override //삭제하는 Dao
 	public int deleteBoard(Connection conn, int boardNo) {
-		String sql = "delete jdbc_board where board_no = ?";
+		String sql = "delete from jdbc_board where board_no = ?";
 		conn = src.util.DBUtil3.getConnection();
 			PreparedStatement pstmt = conn.prepareStatement(sql);
 			pstmt.setInt(1, boardNo);
 			int cnt = pstmt.executeUpdate();
-		
+		if(pstmt!=null)pstmt.close();
 		return cnt;
-	
+	}
 		
 	@Override //검색
 	public List<BoardMemVO> titleSearch(Connection conn, String searching) {
 			String sql = "SELECT * from jdbc_board "
-					+ "			 WHERE LOWER(REPLACE(board_tilte,' ','')) LIKE ? ";
+					+ " WHERE LOWER(REPLACE(board_tilte,' ','')) LIKE '%'|| ? || '%' ";
 		 conn = src.util.DBUtil3.getConnection();	
 		 PreparedStatement pstmt = conn.prepareStatement(sql);
-		 					pstmt.setString(1, searching);
-		 		ResultSet rs  =	pstmt.executeQuery();
+		 pstmt.setString(1, searching);
+		 ResultSet rs  = pstmt.executeQuery();
 		 List<BoardMemVO> list = new ArrayList<BoardMemVO>();
 		 while(rs.next()) {
 			 BoardMemVO memVo = new BoardMemVO();
@@ -134,5 +134,11 @@ public class BoardMemDaoImpl implements BoardMemDao {
 		
 		return list;
 	}
-
+		public int setCountIncrement(Connection conn, int boardNo)
+			String sql = "update jdbc_board set board_cnt = board_cnt +1 where board_no =?";
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			 pstmt.setInt(1, boardNo);
+			int cnt = pstmt.executeUpdate();
+			if(pstmt!=null)pstmt.close();
+			return cnt;
 }
